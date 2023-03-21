42706bab9e7e4a70064fecae619a46dd50ddd048
Steven Adetomiwa <s.adetomiwa@icloud.com>
2023-03-21 01:01:49 -0500

    hw start

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test')
+def hello_test():
+    return 'This is a testKKKK!' 
+
+
+
